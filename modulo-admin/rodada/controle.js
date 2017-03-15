f931004f2a4bcef6d45a969ca02eb625f917a1b3
reinaldovale <reinaldovale@gmail.com>
2017-03-15 00:21:14 -0300

    primeiro commit

--- modulo-admin/rodada/controle.js
+++ modulo-admin/rodada/controle.js
@@ -22,8 +22,8 @@ export default class Rodada {
                 let jogos = Object.keys(jogosGabaritoDeUmaRodadaSnap.val())
                     .map(jogoId => {
                         let jogo = jogosGabaritoDeUmaRodadaSnap.val()[jogoId];
-                        if(jogo.palpites) {
-                          jogo.palpite = jogo.palpites[this.opcoes.usuario];
+                        if (jogo.palpites) {
+                            jogo.palpite = jogo.palpites[this.opcoes.usuario];
                         }
                         jogo.jogoId = jogoId;
                         return jogo;
@@ -35,21 +35,62 @@ export default class Rodada {
 
     salvarPalpites = palpites => {
         let atualizacoes = {},
-        ehGabarito = palpites.ehGabarito;
+            ehGabarito = palpites.ehGabarito;
 
         delete palpites["ehGabarito"];
 
         Object.keys(palpites).map(jogoId => {
-          if(ehGabarito) {
-            atualizacoes[`/gabarito/${jogoId}/mandante/gol`] = palpites[jogoId].mandante.gol;
-            atualizacoes[`/gabarito/${jogoId}/visitante/gol`] = palpites[jogoId].visitante.gol;
-          }
-          else {
-            atualizacoes[`/gabarito/${jogoId}/palpites/${this.opcoes.usuario}`] = palpites[jogoId];
-          }
+            if (ehGabarito) {
+                atualizacoes[`/gabarito/${jogoId}/mandante/gol`] = palpites[jogoId].mandante.gol;
+                atualizacoes[`/gabarito/${jogoId}/visitante/gol`] = palpites[jogoId].visitante.gol;
+
+            } else {
+                atualizacoes[`/gabarito/${jogoId}/palpites/${this.opcoes.usuario}`] = palpites[jogoId];
+            }
         });
         this.servico.salvar(atualizacoes)
-            .then(() => this.buscarJogos(this.opcoes.id))
-            .catch(error => this.visao.exibirMensagem(error));
+            .then(resposta => {
+                if (ehGabarito) {
+                    let atualizacoesPonto = {};
+                    this.servico.buscarJogosDoGabaritoPela(this.opcoes.id)
+                        .then(jogosGabaritoDeUmaRodadaSnap => {
+                            let jogos = Object.keys(jogosGabaritoDeUmaRodadaSnap.val())
+                                .map(jogoId => {
+                                    let jogo = jogosGabaritoDeUmaRodadaSnap.val()[jogoId];
+                                    let pontos = this.calcularPontos(jogo);
+                                });
+                        });
+                }
+                return this.buscarJogos(this.opcoes.id)
+            })
+            .catch(error => {
+                this.visao.exibirMensagem(error)
+            });
+    }
+
+    calcularPontos = (jogo) => {
+      let gm = parseInt(jogo.mandante.gol),
+          gv = parseInt(jogo.visitante.gol);
+          Object.keys(jogo.palpites).map(usuarioId => {
+            let pm = jogo.palpites[usuarioId].mandante.gol,
+            pv = jogo.palpites[usuarioId].visitante.gol;
+            if(gm === pm && gv === pv) return 3;
+          });
+    }
+    simularPontos = (palpite, palpiteSimulado) => {
+        let pm = palpite.placar_mandante,
+            pv = palpite.placar_visitante;
+        if (palpiteSimulado.placar_mandante === "" || palpiteSimulado.placar_visitante === "" || palpite.placar_mandante === "" || palpite.placar_visitante === "") {
+            return 0;
+        } else if (palpiteSimulado.placar_mandante === palpite.placar_mandante && palpiteSimulado.placar_visitante === palpite.placar_visitante) {
+            return 3;
+        } else if (
+            (palpiteSimulado.placar_mandante === palpiteSimulado.placar_visitante && palpite.placar_mandante === palpite.placar_visitante) ||
+            (palpiteSimulado.placar_mandante < palpiteSimulado.placar_visitante && palpite.placar_mandante < palpite.placar_visitante) ||
+            (palpiteSimulado.placar_mandante > palpiteSimulado.placar_visitante && palpite.placar_mandante > palpite.placar_visitante)) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
