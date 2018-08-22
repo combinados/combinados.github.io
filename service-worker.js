2163af2f529ebf607456d0bc6df640b580d516db
combinados <combinadosf@gmail.com>
2018-08-22 15:19:59 -0300

    Mudança de domínio para https://combinados.firebaseapp.com

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-(function () {
+(({ navigator }) => {
   navigator.serviceWorker.getRegistrations().then(function (registrations) {
     for (let registration of registrations) {
       registration.unregister()
     }
   })
-})();
+})(window);
