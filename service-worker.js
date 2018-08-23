7bcd9db8e0b40be03894005487309f7b74df0d40
reinaldovale <reinaldovale@gmail.com>
2018-08-23 01:13:27 -0300

    remove service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
 (() => {
   console.log("self;", self);
-  self.addEventListener("activate", (e) => {
-    self.registration.unregister()
-      .then(() => {
-        return self.clients.matchAll();
-      })
-      .then((clients) => {
-        clients.forEach(client => client.navigate(client.url));
-      });
-  });
+  self.registration.unregister()
+    .then(() => {
+      return self.clients.matchAll();
+    })
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url));
+    });
 })();
